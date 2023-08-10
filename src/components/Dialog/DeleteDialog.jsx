319c5bfefce45a8b5fc70e4a75c30c785c49de24
Salmane <elmourtajisalmane@gmail.com>
2023-08-10 20:07:27 +0100

    created delete request to delete products
    + success alert after delete

--- src/components/Dialog/DeleteDialog.jsx
+++ src/components/Dialog/DeleteDialog.jsx
@@ -3,35 +3,59 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import {Alert, Snackbar, Slide} from '@mui/material';
+import { useState } from 'react';
 
+function DeleteDialog({isOpen, handleClose, onConfirm, state}) {
+  const [openAlert, setOpenAlert] = useState(false);
+  const handleCloseAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+        return;
+    }
+    setOpenAlert(false);
+  }
 
-function DeleteDialog({isOpen, handleClose, onAgree, state}) {
   return (
+  <>
     <Dialog
-        open={isOpen}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title" variant='h4'>
+        Are you sure to delete this item ?
+      </DialogTitle>
+      <DialogContent>
+          <Box sx={{ display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', gap:2, my:3}}>
+            <img alt='product' src={state?.media.thumbUrl} style={{ width:'auto', height:'auto', maxWidth:'90px' }}/>
+            <Typography variant='h5'>{state?.title}</Typography>
+          </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button color='error' onClick={handleClose} autoFocus >Cancel</Button>
+        <Button color='error' onClick={()=> {handleClose(); onConfirm(state.id); setOpenAlert(true)}} >
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+    <Snackbar 
+        open={openAlert} autoHideDuration={5000}
+        onClose={handleCloseAlert} key={state?.id}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        TransitionComponent={SlideTransition}
       >
-        <DialogTitle id="alert-dialog-title" variant='h4'>
-          Are you sure to delete this item ?
-        </DialogTitle>
-        <DialogContent>
-            <Box sx={{ display:'flex', alignItems:'center', flexDirection:'column', gap:2, my:3}}>
-              <img alt='product' src={state?.media.thumbUrl} style={{ width:'auto', height:'auto', maxWidth:'90px' }}/>
-              <Typography variant='h5'>{state?.title}</Typography>
-            </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button color='error' onClick={handleClose} autoFocus >Disagree</Button>
-          <Button color='error' onClick={()=> {handleClose(); onAgree(state.id)}} >
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            Item deleted successfully!
+        </Alert>
+    </Snackbar>
+  </>
   )
+}
+
+function SlideTransition(props) {
+  return <Slide {...props} direction="up" />;
 }
 
 export default DeleteDialog
--- src/pages/Products/SneakersTable.jsx
+++ src/pages/Products/SneakersTable.jsx
@@ -1,23 +1,24 @@
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import DataTable from '../../components/DataTable/DataTable';
 import { useState, useEffect} from 'react';
+import { useGridApiRef } from '@mui/x-data-grid';
+import { useAuthHeader } from 'react-auth-kit';
+import DeleteDialog from '../../components/Dialog/DeleteDialog';
+import DataTable from '../../components/DataTable/DataTable';
 // axios
 import axiosInstance from './../../services/sneakers';
 import useAxiosFunction from './../../hooks/useAxiosFunction';
-import { useGridApiRef } from '@mui/x-data-grid';
-import DeleteDialog from '../../components/Dialog/DeleteDialog';
 
 
 function SneakersTable() {
+  const auth_token = useAuthHeader();
   const [open, setOpen] = useState({isOpen: false, item: null});
-  const apiRef = useGridApiRef();
   const [paginationModel, setPaginationModel] = useState({
     page: 0,
     pageSize: 10,
   });
 
-  const {data: sneakers ,loading, axiosFetch} = useAxiosFunction(axiosInstance);
+  const {data: sneakers, loading,  axiosFetch} = useAxiosFunction(axiosInstance);
 
   useEffect(()=> {
     axiosFetch({
@@ -103,9 +104,19 @@ function SneakersTable() {
   ];
 
   function handleDelete(id) {
-
+    axiosFetch({
+        url:'/' + id,
+        method: 'delete',
+        headers: {
+          'Authorization': auth_token(),
+        },
+        handleResponse: () => axiosFetch({
+          url:`/?page=${paginationModel.page+1}&limit=${paginationModel.pageSize}`,
+          method: 'get',
+        })
+    });
   }
-
+  
   return (
     <>
         <DataTable 
@@ -115,12 +126,11 @@ function SneakersTable() {
             rowCount={sneakers?.total}
             paginationModel={paginationModel}
             setPaginationMode={setPaginationModel}
-            apiRef={apiRef}
         />
         <DeleteDialog 
           isOpen={open.isOpen}
           handleClose={()=> setOpen({...open, isOpen:false})}
-          onAgree={handleDelete}
+          onConfirm={handleDelete}
           state={open.item}
         />
     </>
